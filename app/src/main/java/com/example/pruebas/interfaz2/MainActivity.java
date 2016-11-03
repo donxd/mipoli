c24181e85bcc3553f7723096632cef23e7597dab
donxd <n0s3.xd@gmail.com>
2016-11-03 00:34:45 -0600

    Ajuste presentación diseño - dispositivos

--- app/src/main/java/com/example/pruebas/interfaz2/MainActivity.java
+++ app/src/main/java/com/example/pruebas/interfaz2/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.pruebas.interfaz2;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Spinner;
 
@@ -39,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+
+		ajustaBarraHerramientas();
 
 		drawer = (DrawerLayout) findViewById( R.id.drawer_layout );
 		NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view );
@@ -211,6 +216,39 @@ public class MainActivity extends AppCompatActivity {
 				controlAccesos.setEnabled( true );
 			}
 		});
+	}
+
+	private void ajustaBarraHerramientas (){
+		if ( android.os.Build.VERSION.SDK_INT > 17 ){
+
+			int tamanioBarraHerramientas = getTamanioBarraHerramientas();
+
+			CoordinatorLayout contenedorBarraHerramientas = (CoordinatorLayout) findViewById( R.id.contenedorBarraHerramientas );
+			//contenedorBarraHerramientas.setMinimumHeight( tamanioBarraHerramientas );
+			ViewGroup.LayoutParams parametros = contenedorBarraHerramientas.getLayoutParams();
+			parametros.height = tamanioBarraHerramientas;
+
+			contenedorBarraHerramientas.setLayoutParams( parametros );
+
+			Log.i( "Info",  "Actualización tamanio barra de herramientas : " + tamanioBarraHerramientas );
+		}
+
+	}
+
+	private int getTamanioBarraHerramientas (){
+
+		int tamanioBarraHerramientas = getDimenBarraHerramientas();
+
+		/*switch ( tamanioBarraHerramientas ){
+			case
+		}*/
+
+		return tamanioBarraHerramientas;
+
+	}
+
+	private int getDimenBarraHerramientas (){
+		return (int) getResources().getDimension(R.dimen.barra_herramientas);
 	}
 
 }
